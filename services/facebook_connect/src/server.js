d17bd57cbd691825a88720b83a80f9f14707afb3
Roy Niederberg <roy.niedeberg@gmail.com>
2025-11-07 16:12:05 +0200

    debug prod

--- services/facebook_connect/src/server.js
+++ services/facebook_connect/src/server.js
@@ -12,7 +12,7 @@ const post_fields_list = 'fields=id,message,from,created_time'
 
 app.use(express.json());
 const LOG = (e) => { console.log(`🚨 ERROR 🚨 : ${e}`); return true }
-const format_comment = (comment) => `<<<${comment.created_time}>>> <<<${comment.from?.name}>>>: ${comment.message}`
+const format_mssg = (cmt) => `<<<${cmt.created_time}>>> <<<${cmt.from?.name}>>>: ${cmt.message}`
 
 
 app.post('/', async (req, res) => {
@@ -31,54 +31,48 @@ app.post('/', async (req, res) => {
       if (change.value?.item === 'comment'
         && change.value.from?.id !== pageId
         && change.value.verb === 'add'
-      ) process_comment(change.value)
+      ) process_comment(change.value.comment_id, change.value.post_id)
     })
   })
 })
 
-async function process_comment(comment) {
-  const { comment_id, post_id } = comment
-  if (comment.parent_id === comment.post_id) comment.parent_id = undefined;
-  comment.created_time = new Date(comment.created_time * 1000).toISOString()
-  const chat_history = [format_comment(comment)]
+async function process_comment(comment_id, post_id) {
+  const chat_history = []
+  const orig_comment_id = comment_id
 
-  console.log('===========================================================')
-  console.log(comment)
-  console.log('===========================================================')
-
-
-  while (comment.parent_id) {
-    console.log(comment.comment_id + " => " + comment.parent_id)
-    const url = `${fb_url}/${comment.parent_id}?${comment_fields_list}&access_token=${token}`
+  // Fetch comments history
+  while (comment_id) {
+    const url = `${fb_url}/${comment_id}?${comment_fields_list}&access_token=${token}`
     const ret = await fetch(url)
     if (!ret.ok && LOG(`3 ${ret.status} ${ret.statusText} ${await ret.text()}`)) return
     comment = await ret.json();
-    chat_history.unshift(format_comment(comment))
+    chat_history.unshift(format_mssg(comment))
+    comment_id = comment.parent?.id
   }
 
+  // Fetch the Post
   const url = `${fb_url}/${post_id}?${post_fields_list}&access_token=${token}`
   const ret = await fetch(url)
   if (!ret.ok && LOG(`6 ${ret.status} ${ret.statusText} ${await ret.text()}`)) return
   const post = await ret.json()
-  chat_history.unshift(format_comment(post))
+  chat_history.unshift(format_mssg(post))
 
+  // Create the query and ask the prompt-composer for answer
   let query = `# CHAT METADATA:\nThe chat is from the business Facebook page and you are replaying on a comment thread on a public post. Your previous messages on the chat will appear under the name "${post.from?.name}"\n\n`
   query = query + "# CHAT\n" + chat_history.join('\n') + "\n\n"
-  console.log('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
   console.log(query)
-  console.log('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
 
   const llm_ret = await fetch(`http://prompt-composer:4321/ask?query=${encodeURIComponent(query)}`)
   if (!llm_ret.ok && LOG(`4 ${llm_ret.status} ${llm_ret.statusText}`)) return
   const answer = await llm_ret.text()
 
-  const reply_url = `${fb_url}/${comment_id}/comments?message=${encodeURIComponent(answer)}&access_token=${token}`
+  // Feplay on the original comment
+  const reply_url = `${fb_url}/${orig_comment_id}/comments?message=${encodeURIComponent(answer)}&access_token=${token}`
   const reply_response = await fetch(reply_url, { method: 'POST' })
   if (!reply_response.ok && LOG(`5 ${reply_response.status} ${reply_response.statusText}`)) return
   const reply_data = await reply_response.json()
   console.log(`✅ Reply posted to Facebook (ID: ${reply_data.id})`)
 }
-
 
 app.listen(3210, ()=> console.log('Server Start Up'))
 //--------------------------------------------------------------------------------------------------
